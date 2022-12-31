d466b8aee1995ad3ea2af35eefcaebb037255550
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2022-12-31 21:27:20 +0800

    提交Button函数测试

---
+++ src/mega/Interact/Button/unit/Button.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {fireEvent, render} from "@testing-library/react";
+import {HandleNumberWithSuffix} from "../ButtonFunc";
+
+describe('functionTest', ()=>{
+    it('testNumberWithSuffix', ()=> {
+        const rem = HandleNumberWithSuffix('12rem', 4);
+        expect(rem).toEqual('3rem');
+        const px = HandleNumberWithSuffix('200px',5);
+        expect(px).toEqual('40px');
+        const vmin = HandleNumberWithSuffix('400vmin',4);
+        expect(vmin).toEqual('100vmin');
+        const vw = HandleNumberWithSuffix('24vw',6);
+        expect(vw).toEqual('4vw');
+        const vh = HandleNumberWithSuffix('36vh',6);
+        expect(vh).toEqual('6vh');
+    })
+})
